3f3d438a2392c80cbacd975c6d2410a6ed6e5170
Paul Correll <pcorrell@users.noreply.github.com>
2019-05-02 12:10:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const vm = new Vue({
     selected: ''
   },
   mounted() {
-    axios.get("https://newsapi.org/v2/everything -G -d q=fauquier -d apiKey=3f0d145189a442debea66f9614883f40")
+    axios.get("https://newsapi.org/v2/everything?q=Fauquier&apiKey=3f0d145189a442debea66f9614883f40")
     .then(response => {this.results = response.data.articles})
   }
 });
